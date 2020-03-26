782f652913d6f03495b48a531a6d38b77ae548da
nusretozates <nozatespc@gmail.com>
2020-03-26 22:21:21 +0300

    asasaads

--- ChatBot/views.py
+++ ChatBot/views.py
@@ -9,9 +9,21 @@ import ast
 
 def ChatBotMessage(request):
 
+    text = request.POST["chat"]
     client = Wit('HFLSFMYE7ZZCYSHSFAAWMGDAEJ237CWO')
-    resp = client.message("Hello World")
+    resp = client.message(text)
     x = str(resp)
     y = ast.literal_eval(x)
 
-    return HttpResponse(y["entities"])
+    intent = y["entities"]["intent"][0]["value"]
+    subValue = y["entities"]["vergessenesElement"][0]["value"]
+
+
+    switcher = {
+        "zuruecksetzen" : "Um Ihr Passwort zurückzusetzen, folgen Sie folgenden Anweisungen unter www.mechatroniker.com.tr/pw-zurucksetzen",
+        "begruessung" : "Hallo, ich bin Chatbotcan, wie kann ich Ihnen behilflich sein?",
+        "anmeldeprobleme" : "Falls Sie Probleme mit dem Anmelden in das Intranet haben, benötigen Sie Ihre Daimler-Nutzerkennung, die Sie durch ein anderes Teammitglied erfahren können. Anschließend können Sie über www.mechatroniker.com.tr/pw-zurucksetzen Ihr Passwort zurücksetzen"
+    }
+
+
+    return HttpResponse(switcher[intent])
